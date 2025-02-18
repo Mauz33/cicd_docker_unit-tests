d11d6fccd3a539241be9f06c001aeba6ca1b024e
Maxim Eshenko <eshenko.maxim@gmail.com>
2025-02-18 18:06:24 +0300

    requirements

--- test.py
+++ test.py
@@ -1,14 +1,8 @@
-import unittest
-
+import pytest
 
 def add(x, y):
     return x + y
 
 
-class TestMath(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(5, 14), 19)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add():
+    assert add(5, 14) == 19
